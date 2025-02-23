576dc67081cad943ed443de2eec4e2886668cd15
Luke Loecher <ljloecher@gmail.com>
2025-02-23 16:52:20 -0500

    update api urls

--- src/api/getPastOrder.js
+++ src/api/getPastOrder.js
@@ -1,6 +1,6 @@
 export default async function getPastOrder(order) {
   const apiURl = import.meta.env.VITE_API_URL;
-  const response = await fetch(`${apiURl}/past-order/${order}`);
+  const response = await fetch(`${apiURl}/api/past-order/${order}`);
   const data = await response.json();
   return data;
 }
